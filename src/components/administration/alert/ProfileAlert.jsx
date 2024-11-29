5096f53115f00e201879b9e1877063f13a77e0e1
Copter <motto5465@gmail.com>
2024-11-29 15:40:46 +0700

    fix. - Profile Card Logic for Admin

--- src/components/administration/alert/ProfileAlert.jsx
+++ src/components/administration/alert/ProfileAlert.jsx
@@ -35,7 +35,7 @@ const ProfileAlert = ({ isOpen, isClose, onClose, children, setStudent, EditCour
     axios.put(
       `${
         process.env.REACT_APP_SERVER_DOMAIN_NAME
-      }/api/student/update-ta?student_id=${children.student_id}&is_ta=${
+      }/api/student/update-ta?student_id=${children.id}&is_ta=${
         inputTACourse ? "true" : "false"
       }&ta_course_id=${inputTACourse ? inputTACourse : "null"}`,
       {},
@@ -71,7 +71,7 @@ const ProfileAlert = ({ isOpen, isClose, onClose, children, setStudent, EditCour
   function handleRegCourse() {
     axios.post(`${process.env.REACT_APP_SERVER_DOMAIN_NAME}/api/student/register-course`, {
       "course_id": inputCourse,
-      "student_id": children.student_id,
+      "student_id": children.id,
       "year": children.year
     }, {
       headers: {
@@ -99,7 +99,7 @@ const ProfileAlert = ({ isOpen, isClose, onClose, children, setStudent, EditCour
   }
 
   function handleRemoveCourse(course_id) {
-    axios.delete(`${process.env.REACT_APP_SERVER_DOMAIN_NAME}/api/student/withdraw-course?student_id=${children.student_id}&course_id=${course_id}`, {
+    axios.delete(`${process.env.REACT_APP_SERVER_DOMAIN_NAME}/api/student/withdraw-course?student_id=${children.id}&course_id=${course_id}`, {
       headers: {
         "x-token": localStorage.getItem("token"),
       }
--- src/components/administration/alert/TableCourses.jsx
+++ src/components/administration/alert/TableCourses.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaTrash } from "react-icons/fa6";
 
 function TableCourses({ student, isEditCourses, onDelete }) {
+    console.log(student)
 
     return (
         <table className='col-span-2 w-full mt-4 table-auto border-gray-500 border-collapse'>
@@ -13,13 +14,13 @@ function TableCourses({ student, isEditCourses, onDelete }) {
                 </tr>
             </thead>
             <tbody>
-                { student.registered_courses.map((course) => {
+                { student.registered.map((enroll) => {
                     return (
-                        <tr key={course.course_id} className='h-[2.5rem] hover:bg-orange-50 transition-colors duration-150 border-b border-gray-300 last:border-b-0 bg-gray-50'>
-                            <td className='p-2 border-r-2 border-gray-400 text-sm text-gray-700 text-center'>{course.course_id}</td>
-                            <td className='p-2 text-sm text-gray-700'>{course.name}</td>
+                        <tr key={enroll.course.id} className='h-[2.5rem] hover:bg-orange-50 transition-colors duration-150 border-b border-gray-300 last:border-b-0 bg-gray-50'>
+                            <td className='p-2 border-r-2 border-gray-400 text-sm text-gray-700 text-center'>{enroll.course.id}</td>
+                            <td className='p-2 text-sm text-gray-700'>{enroll.course.name}</td>
                             <td className={`p-1 border-l-2 border-gray-400 text-sm text-gray-700 ${ isEditCourses ? "" : "hidden" }`}>
-                                <div className='w-7 p-2 m-auto hover:bg-red-100 hover:cursor-pointer rounded-full transition duration-150' onClick={() => onDelete(course.course_id, course.name)} >
+                                <div className='w-7 p-2 m-auto hover:bg-red-100 hover:cursor-pointer rounded-full transition duration-150' onClick={() => onDelete(enroll.course.id, enroll.course.name)} >
                                     <FaTrash className='text-red-500 m-auto' />
                                 </div>
                             </td>
